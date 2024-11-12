94450e95b7bfdc35e6f2ff0039a81567fbe43e7c
Gabriel Felipe <gabrielfelipea2000@icloud.com>
2024-11-11 23:37:57 -0300

    create header and footer component

---
+++ src/shared/components/header.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <p>Este é o conteúdo que mudará com o modo escuro.</p>
+    <ButtonTheme></ButtonTheme>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import ButtonTheme from './theme/buttonTheme.vue';
+
+</script>
