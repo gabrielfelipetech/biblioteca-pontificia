29f195d5fe5f02aa0df2654583a7a1615b813523
Gabriel Felipe <gabrielfelipea2000@icloud.com>
2024-11-11 23:38:47 -0300

    adjust nuxtconfig

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,16 @@
+import { defineNuxtConfig } from "nuxt/config";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  css: ['@/assets/scss/tailwind.scss', '@/assets/scss/global.scss',],
   srcDir: 'src/',
+  app: {
+    head: {
+      title: 'Biblioteca Pontificia',
+    },
+  },
   modules: [
     '@nuxt/devtools',
     '@nuxt/test-utils',
