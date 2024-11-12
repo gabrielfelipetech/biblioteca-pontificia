fdfd50bc01356bd4405043b2ebbc82d99d718bf8
Gabriel Felipe <gabrielfelipea2000@icloud.com>
2024-11-11 23:37:37 -0300

    create button theme

---
+++ src/shared/components/theme/buttonTheme.vue
@@ -0,0 +1,24 @@
+<template>
+  <button @click="toggleDarkMode">
+    {{ isDark }}
+  </button>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const isDark = ref(false)
+
+const toggleDarkMode = () => {
+  isDark.value = !isDark.value
+  if (isDark.value) {
+    document.documentElement.classList.add('dark')
+  } else {
+    document.documentElement.classList.remove('dark')
+  }
+}
+</script>
+
+<style>
+
+</style>
