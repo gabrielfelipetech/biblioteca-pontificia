9c1ad39dc1a46bc01eb660e44b3e4893b4f76cb9
Gabriel Felipe <gabrielfelipea2000@icloud.com>
2024-11-14 00:10:05 -0300

    create header component

--- src/shared/components/header.vue
+++ src/shared/components/header.vue
@@ -1,11 +1,15 @@
 <template>
-  <div>
-    <p>Este é o conteúdo que mudará com o modo escuro.</p>
-    <ButtonTheme></ButtonTheme>
-  </div>
+  <header class="dark:border-gold-medium border-blue-medium border-b flex py-3 px-6 justify-between align-middle">
+    <h1>Biblioteca Pontificia</h1>
+    <Input v-model="searchInput"/>
+    <ButtonTheme/>
+  </header>
 </template>
 
 <script lang="ts" setup>
 import ButtonTheme from './theme/buttonTheme.vue';
+import Input from './atom/input.vue'
+import { ref } from 'vue';
 
+const searchInput = ref('')
 </script>
