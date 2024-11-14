d2cbf62263541a2c2f07e6f2711d7d16d9e98db7
Gabriel Felipe <gabrielfelipea2000@icloud.com>
2024-11-14 00:09:25 -0300

    create input component

---
+++ src/shared/components/atom/input.vue
@@ -0,0 +1,42 @@
+<template>
+  <input
+    :type="type"
+    :placeholder="placeholder"
+    :value="modelValue"
+    class="input-class"
+    @input="onInput"
+  >
+</template>
+
+<script lang="ts" setup>
+import { defineProps, defineEmits } from 'vue';
+
+type BaseInputProps = {
+  modelValue: string | number;
+  placeholder?: string;
+  type?: string;
+};
+
+type BaseInputEmits = {
+  (e: 'update:modelValue', value: string | number): void;
+};
+
+defineProps<BaseInputProps>();
+const emits = defineEmits<BaseInputEmits>();
+
+function onInput(event: Event) {
+  const target = event.target as HTMLInputElement;
+  emits('update:modelValue', target.value);
+}
+</script>
+
+<style scoped>
+.input-class {
+  @apply h-10 dark:border-gold-deep dark:bg-blue-medium border-1 rounded-lg bg-gold-light border-blue-deep outline-none appearance-none text-base text-blue-deep dark:text-gold-deep px-2;
+}
+.input-class:hover,
+.input-class:focus {
+  @apply border-black dark:border-gold-soft;
+  @apply text-blue-medium dark:text-gold-medium
+}
+</style>
