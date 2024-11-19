ff140fb2153be4f4c40668e1f081d2bd46f681bd
Gabriel Felipe <gabrielfelipea2000@icloud.com>
2024-11-19 12:38:45 -0300

    adjust name of components

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,10 +1,13 @@
 <template>
     <div class="">
-        <Header></Header>
-        <Footer></Footer>
+        <BPHeader />
+        <BPFooter />
+        <SideBar />
     </div>
 </template>
 <script setup lang="ts">
-import Footer from '@/shared/components/footer.vue';
-import Header from '@/shared/components/header.vue';
+import BPFooter from '~/shared/components/organism/BPFooter.vue';
+import BPHeader from '~/shared/components/organism/BPHeader.vue';
+import SideBar from '@/shared/components/organism/sideBar.vue';
+
 </script>
