ae68f2a005e1e9b0ec218b43bb1156ef082c6c06
Gabriel Felipe <gabrielfelipea2000@icloud.com>
2024-11-21 00:20:37 -0300

    add rules for eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,28 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from './.nuxt/eslint.config.mjs';
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default withNuxt([
+  {
+    files: ['**/*.{js,ts,vue}'],
+
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+
+    rules: {
+      'no-console': 'warn',
+      'no-debugger': 'error',
+      eqeqeq: ['error', 'always'],
+      curly: ['error', 'all'],
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
+      indent: ['error', 2],
+      'comma-dangle': ['error', 'always-multiline'],
+      'vue/html-indent': ['error', 2],
+      'vue/max-attributes-per-line': ['warn', { singleline: 3 }],
+    },
+  },
+]);
