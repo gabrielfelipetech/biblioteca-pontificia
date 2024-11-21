d3cb3eb3cc11db3227b709b9d6f56d136833e8c0
Gabriel Felipe <gabrielfelipea2000@icloud.com>
2024-11-21 00:23:44 -0300

    create style bp-header

--- src/shared/components/organism/BPHeader.vue
+++ src/shared/components/organism/BPHeader.vue
@@ -1,15 +1,22 @@
 <template>
-  <header class="dark:border-gold-medium border-blue-medium border-b flex py-3 px-6 justify-between items-center">
+  <header
+    class="dark:bg-bp-blue-800-light bg-bp-yellow-100-light dark:border-bp-yellow-100-light border-bp-blue-800-light border-b flex py-2 px-6 justify-between items-center"
+  >
     <h1>Biblioteca Pontificia</h1>
-    <Input v-model="searchInput" icon-name="mdi:magnify" class="w-80" placeholder="Pesquisar Leitura"/>
-    <ButtonTheme/>
+    <Input
+      v-model="searchInput"
+      icon-name="mdi:magnify"
+      class="w-80"
+      placeholder="Pesquisar Leitura"
+    />
+    <UserMenu />
   </header>
 </template>
 
 <script lang="ts" setup>
-import ButtonTheme from '../theme/buttonTheme.vue';
-import Input from '../atom/BPInput.vue'
+import Input from '../atom/BPInput.vue';
 import { ref } from 'vue';
+import UserMenu from './userMenu.vue';
 
-const searchInput = ref('')
+const searchInput = ref('');
 </script>
