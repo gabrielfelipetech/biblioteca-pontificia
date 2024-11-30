4c04c31ee269e7c3c124aec9ed56b54dd16bc1e7
Gabriel Felipe <gabrielfelipea2000@icloud.com>
2024-11-29 21:02:12 -0300

    create button theme

--- src/shared/components/theme/buttonTheme.vue
+++ src/shared/components/theme/buttonTheme.vue
@@ -1,18 +1,22 @@
 <template>
-  <button
-    class="flex items-center gap-4 justify-center"
+  <BPButton
+    :icon-name="isDark ? 'mdi:weather-sunny' : 'mdi:weather-night'"
+    :button-text="isDark ? 'Claro' : 'Escuro'"
+    class="button-theme"
+    :class="isDark ? 'flex-row-reverse' : 'flex'"
     @click="toggleDarkMode"
-  >
-    <Icon v-if="!isDark" name="mdi:weather-sunny" />
-    {{ isDark ? 'Escuro' : 'Claro' }}
-    <Icon v-if="isDark" name="mdi:weather-night" />
-  </button>
+  />
 </template>
 
 <script lang="ts" setup>
 import { useDarkMode } from '@/shared/composables/useDarkMode';
+import BPButton from '../atom/BPButton.vue';
 
 const { isDark, toggleDarkMode } = useDarkMode();
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.button-theme {
+  @apply flex justify-center gap-4 items-center;
+}
+</style>
